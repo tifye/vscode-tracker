1e3c1e3ad4f355b166f21c546064fc9fd317e78b
tifufu <dematasjoshua@hotmail.com>
2025-07-05 22:57:41 +0200

    init: base extension

---
+++ extension.js
@@ -0,0 +1,235 @@
+const vscode = require('vscode')
+
+/** @type {NodeJS.Timeout | undefined} */
+let statePollInterval
+
+/** State of activity
+ * @typedef {Object} State
+ * @property {string} workspace
+ * @property {string} fileName
+ * @property {string} language
+ * @property {number} row
+ * @property {number} col
+ * @property {string} viewChunk
+ */
+
+/** @type {State} */
+let prevState = {
+    workspace: '',
+    fileName: '',
+    language: '',
+    row: 0,
+    col: 0,
+    viewChunk: '',
+}
+
+/** @type {AbortController | undefined} */
+let updateStateAbortController
+
+/** @type {vscode.OutputChannel} */
+let chan
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+    chan = vscode.window.createOutputChannel('Activity Tracker')
+
+    const target = context.globalState.get('target')
+    context.secrets.get('token').then((token) => {
+        if (token === undefined) {
+            chan.appendLine('No token found')
+            return
+        }
+
+        setInterval(() => pollState(target, token), 2000)
+    })
+
+    const disposeSetTarget = vscode.commands.registerCommand(
+        'activity-tracker.setTarget',
+        () => handleSetTargetCmd(context),
+    )
+
+    const disposeSetToken = vscode.commands.registerCommand(
+        'activity-tracker.setToken',
+        () => handleSetTokenCmd(context),
+    )
+
+    context.subscriptions.push(
+        disposeSetTarget,
+        disposeSetToken,
+        {
+            dispose: chan.dispose,
+        },
+        {
+            dispose: () => clearInterval(statePollInterval),
+        },
+    )
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {
+    clearInterval(statePollInterval)
+    if (chan !== undefined) {
+        chan.dispose()
+    }
+    if (updateStateAbortController !== undefined) {
+        updateStateAbortController.abort()
+    }
+}
+
+/**
+ *
+ * @param {string} target
+ * @param {string} token
+ * @returns
+ */
+async function pollState(target, token) {
+    const doc = vscode.window.activeTextEditor.document
+    const sel = vscode.window.activeTextEditor.selection
+    const view = doc.getText(getViewRange(sel, doc.lineCount))
+
+    // todo: use git-ignore
+    if (doc.fileName.includes('.env')) {
+        return
+    }
+
+    /** @type {State} */
+    const curState = {
+        workspace: vscode.workspace.name,
+        fileName: doc.fileName,
+        language: doc.languageId,
+        row: sel.active.line,
+        col: sel.active.character,
+        viewChunk: view,
+    }
+
+    if (shallowEqual(prevState, curState)) {
+        return
+    }
+
+    prevState = curState
+    chan.appendLine('Updating state')
+
+    if (updateStateAbortController !== undefined) {
+        updateStateAbortController.abort()
+    }
+
+    updateStateAbortController = new AbortController()
+    const url = target
+    try {
+        const resp = await fetch(url, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(curState),
+            signal: updateStateAbortController.signal,
+        })
+
+        if (!resp.ok) {
+            throw new Error(await resp.text())
+        }
+    } catch (err) {
+        chan.appendLine(`Something went wrong: ${err}`)
+    }
+}
+
+/**
+ *
+ * @param {vscode.Selection} selection
+ * @param {number} lineCount
+ * @returns {vscode.Range}
+ */
+function getViewRange(selection, lineCount) {
+    const range = 5
+    const totalRange = range * 2 + 1
+
+    if (lineCount < totalRange) {
+        return new vscode.Range(
+            new vscode.Position(0, 0),
+            new vscode.Position(lineCount, 0),
+        )
+    }
+
+    let startLine = Math.max(0, selection.active.line - range)
+    if (lineCount - startLine >= totalRange) {
+        return new vscode.Range(
+            new vscode.Position(startLine, 0),
+            new vscode.Position(startLine + totalRange - 1, 0),
+        )
+    }
+
+    startLine = startLine - Math.abs(lineCount - startLine - totalRange)
+    return new vscode.Range(
+        new vscode.Position(startLine, 0),
+        new vscode.Position(startLine + totalRange - 1, 0),
+    )
+}
+
+function shallowEqual(objA, objB) {
+    const keysA = Object.keys(objA)
+    const keysB = Object.keys(objB)
+
+    if (keysA.length !== keysB.length) return false
+
+    for (let key of keysA) {
+        if (objA[key] !== objB[key]) return false
+    }
+
+    return true
+}
+
+/**
+ *
+ * @param {vscode.ExtensionContext} context
+ */
+function handleSetTokenCmd(context) {
+    vscode.window
+        .showInputBox({
+            password: true,
+            prompt: 'Set authorization bearer token used when making requests to target backend.',
+            title: 'Bearer token',
+        })
+        .then((token) => {
+            if (token === undefined || token.trim() === '') {
+                vscode.window.showErrorMessage('No token provided.')
+
+                return
+            }
+
+            context.secrets.store('token', token)
+            vscode.window.showErrorMessage(
+                'Token updated, extension restart requried.',
+            )
+        })
+}
+
+/**
+ *
+ * @param {vscode.ExtensionContext} context
+ */
+function handleSetTargetCmd(context) {
+    vscode.window
+        .showInputBox({
+            prompt: 'Set target backend.',
+            title: 'Target backend',
+        })
+        .then((target) => {
+            if (target === undefined || target.trim() === '') {
+                vscode.window.showErrorMessage('No target provided.')
+                return
+            }
+
+            context.secrets.store('target', target)
+            vscode.window.showErrorMessage(
+                'Target updated, extension restart requried.',
+            )
+        })
+}
+
+module.exports = {
+    activate,
+    deactivate,
+}
